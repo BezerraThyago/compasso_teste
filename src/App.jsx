842a33b8c23547fcb6ce0023f2bfc8b29508bb18
Thyago Bezerra da silva <bezerrathyago@gmail.com>
2021-04-27 00:10:23 -0300

    Refactoring and Styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,7 @@
 import "bootstrap/dist/css/bootstrap.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
 import "./App.scss";
-import Header from "./components/Header/Header";
-import Erro404 from "./pages/Erro404/Erro404";
-import Home from "./pages/Home/Home";
-import User from "./pages/User/User";
+import AppRoutes from "./pages";
 
-function App() {
-  return (
-    <Router>
-      <Header />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/:userAccount">
-          <User />
-        </Route>
-        <Route exac path="/404"t>
-          <Erro404 />
-        </Route>
-      </Switch>
-    </Router>
-  );
-}
+const App = () => <AppRoutes />;
 
 export default App;
---
+++ src/pages/index.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import EmptySearch from "./EmptySearch";
+import Home from "./Home";
+import User from "./User";
+
+const AppRoutes = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/:userAccount">
+          <User />
+        </Route>
+        <Route path="/empty-search" t>
+          <EmptySearch />
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
+
+export default AppRoutes;
